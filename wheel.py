1a14214c3b03556914b8a723de4898c5ba2e5acc
Andrew Lyko <marian.lyko@gmail.com>
2021-05-24 21:10:59 +0200

    solve according to comments

--- wheel.py
+++ wheel.py
@@ -3,6 +3,6 @@ from random import randint
 wheel = [200, 300, 500, 1000, 100, 50, 0, -1, 200, 300, 500, 1000, 100, 50, 200, 300, 500, 1000, 100, 50]
 
 
-def spine_wheel():
-    return wheel[randint(0, len(wheel))]
+def spin_wheel():
+    return wheel[randint(0, len(wheel) - 1)]
 
